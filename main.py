d54596ec729ae3a398237314cf400d8f397f4924
Neto test <ibutov6@yandex.ru>
2024-02-08 21:22:02 +0700

    second commit

---
+++ main.py
@@ -0,0 +1,11 @@
+main.py
+
+import datetime as dt
+
+from application.db.people import get_employees
+from application.salary import calculate_salary
+
+if __name__ == '__main__':
+    print(f'Today is {dt.date.today()}')
+    calculate_salary()
+    get_employees()
